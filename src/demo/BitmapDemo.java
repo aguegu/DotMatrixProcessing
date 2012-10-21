8517e6dfb2a253ba383b950d7c0d13a92a993e6c
Weihong.Guan <weihong.Guan@gmail.com>
2012-10-22 02:41:37 +0800

    add BitmapDemo

--- src/demo/BitmapDemo.java
+++ src/demo/BitmapDemo.java
@@ -1,19 +1,31 @@
 package demo;
 
-import java.util.Arrays;
-
 import processing.core.PApplet;
+
+import java.util.Arrays;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+// import processing.serial.*;
 
 public class BitmapDemo extends PApplet
 {
 	private static final long serialVersionUID = -8270200464164013L;
 
 	private byte[] _cache;
+	// private Serial _sp;
+	Minim _minim;
+	AudioPlayer _ap;
 
 	public void setup()
 	{
+		// _sp = new Serial(this, "/dev/ttyACM0", 115200);
+
 		this.size(128, 64);
 		this.background(0xff);
+
+		_minim = new Minim(this);
+		_ap = _minim.loadFile("sample.mp3", 128);
+		_ap.loop();
 
 		this.loadPixels();
 		_cache = new byte[this.pixels.length / 8];
@@ -23,18 +35,27 @@ public class BitmapDemo extends PApplet
 
 		this.calcCache();
 		this.printCache();
+
+
 	}
 
 	public void draw()
 	{
-
+		background(0xff);
+		for (int i = 0; i < _ap.mix.size(); i++)
+		{
+			point(i, height / 2 + _ap.mix.get(i) * height);
+		}
+		this.calcCache();
+		// _sp.write(0xf3);
+		// _sp.write(_cache);
 	}
 
 	public void stop()
 	{
 		// always close Minim audio classes when you finish with them
-		// ap.close();
-		// minim.stop();
+		_ap.close();
+		_minim.stop();
 		super.stop();
 	}
 
